520e17c45499a132099142326282450397464111
asemali <asemali2210@gmail.com>
2024-11-02 23:58:03 +0200

    fix feature main

--- frontend/src/components/layout/FeatureMainSection.jsx
+++ frontend/src/components/layout/FeatureMainSection.jsx
@@ -1,31 +1,44 @@
 import Link from "next/link"
 
-export default function FeatureMainSection({data}) {
-  console.log(data,"55555555")
-  const {feature } = data
+export default function FeatureMainSection({ data }) {
+  console.log(data);
 
   return (
     <div className="feature__main">
         <div className="container">
           <div className="row">
-            {
-              feature.map()
-            }
-            <div className="col-md-6">
-              <div className="feature__main">
-                <div className="_left">
-                  <p className="__tag">Free delivery</p>
-                  <p className="heading">Free delivery over $50 </p>
-                  <p className="sub__heading">Shop $50 product and get free delivery anywhre.</p>
-                  <Link href='/'>
-                    Shop Now
-                  </Link>
+          
+                <div className="col-md-6">
+                  <div className="feature__main">
+                    <div className="_left">
+                      <p className="__tag">Free delivery</p>
+                      <p className="heading">Free delivery over $50 </p>
+                      <p className="sub__heading">Shop $50 product and get free delivery anywhre.</p>
+                      <Link href='/'>
+                        Shop Now
+                      </Link>
+                    </div>
+                    <div className="_right">
+                      
+                    </div>
+                  </div>
                 </div>
-                <div className="_right">
-                  
+                <div className="col-md-6">
+                  <div className="feature__main">
+                    <div className="_left">
+                      <p className="__tag">Free delivery</p>
+                      <p className="heading">Free delivery over $50 </p>
+                      <p className="sub__heading">Shop $50 product and get free delivery anywhre.</p>
+                      <Link href='/'>
+                        Shop Now
+                      </Link>
+                    </div>
+                    <div className="_right">
+                      
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+            
           </div>
         </div>
     </div>
