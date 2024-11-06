18677e6017396906a24ee06f530eb3d05e062d72
asemali <asemali2210@gmail.com>
2024-11-06 23:57:25 +0200

    slae section 3

--- frontend/src/components/custom/Sale.jsx
+++ frontend/src/components/custom/Sale.jsx
@@ -13,6 +13,15 @@ export default  function Sale() {
                     <p className="_category">$20</p>
                     <button >Add to cart</button>
                 </div>
+                <div className="form">
+                  <p className="heading">10% OFF</p>
+                  <p className="sub__heading">For new member sign up at the first time</p>
+                  <label>Email address*</label>
+                  <input className="" type="email" placeholder="johndoe@gmail.com"></input>
+                  <label>Password</label>
+                  <input className="" type="email" placeholder="johndoe@gmail.com"></input>
+                  <button type="submit">Register Now</button>
+                </div>
             </div>
         </div>
     </div>
